ec392ec24234d1bc0fab395530632fafacbcbeac
Marcin Soltysik <profittax@gmail.com>
2023-04-03 22:56:51 +0200

    feat(guess-my-number): Add displayMessage function, refactor checkNumber

--- 01-guess-my-number/script.js
+++ 01-guess-my-number/script.js
@@ -8,18 +8,27 @@ const message = document.querySelector('.game-info-section__message');
 let secretNumber = Math.floor(Math.random() * 20) + 1;
 mysteryNumber.textContent = secretNumber;
 
+const displayMessage = function (notice) {
+  message.textContent = notice;
+};
+
 checkNumber.addEventListener('click', function () {
   const guess = Number(guessNumber.value);
 
   if (!guess) {
-    message.textContent = '🫤 No number! Enter a Number';
+    // message.textContent = '🫤 No number! Enter a Number';
+    displayMessage('🫤 No number! Enter a Number');
   } else if (guess === secretNumber) {
-    message.textContent = '😀 Correct Number! You WIN 🥳';
+    // message.textContent = '😀 Correct Number! You WIN 🥳';
+    displayMessage('😀 Correct Number! You WIN 🥳');
   } else if (guess > secretNumber) {
-    message.textContent = '⬆️ Too hight!';
+    // message.textContent = '⬆️ Too hight!';
+    displayMessage('⬆️ Too hight!');
   } else if (guess < secretNumber) {
-    message.textContent = '⬇️ Too low!';
+    // message.textContent = '⬇️ Too low!';
+    displayMessage('⬇️ Too low!');
   } else {
-    message.textContent = '😭 You lost the game.';
+    // message.textContent = '😭 You lost the game.';
+    displayMessage('😭 You lost the game.');
   }
 });
