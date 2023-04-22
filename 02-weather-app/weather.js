32fdaaee8a4f52d77412ea56b4c5f90294aead42
Marcin Soltysik <profittax@gmail.com>
2023-04-22 21:22:41 +0200

    feat(weather-app): Add async function, for get data

--- 02-weather-app/weather.js
+++ 02-weather-app/weather.js
@@ -2,11 +2,20 @@
 const key = '6ad0d5e302ac0d03fcc31a072356981f';
 
 const inputCity = document.querySelector('.input');
-function search(ev) {
-  const phrase = ev.target.value;
-  console.log(phrase);
+
+async function search() {
+  const phrase = inputCity.value;
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${phrase}&limit=5&appid=${key}`
+  );
+  const data = await response.json();
+  console.log(data);
 }
 
-inputCity.addEventListener('keyup', search);
+const debounceSearch = _.debounce(() => {
+  search();
+}, 600);
+
+inputCity.addEventListener('keyup', debounceSearch);
 
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
