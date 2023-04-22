2c99fcb45960d7bfa8ed28df866975c0d1e93b61
Marcin Soltysik <profittax@gmail.com>
2023-04-22 21:45:46 +0200

    feat(weather-app): Add list display for search with data-attribute

--- 02-weather-app/weather.js
+++ 02-weather-app/weather.js
@@ -8,8 +8,25 @@ async function search() {
   const response = await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${phrase}&limit=5&appid=${key}`
   );
-  const data = await response.json();
+  const data = await response.json(); // pobrales dane, teraz cos trzeba z nimi zrobic
   console.log(data);
+  const ul = document.querySelector('.input--list');
+  ul.innerHTML = ''; //czyscisz liste
+  // teraz iteracja po tablicy z danymi ktore pobrales w aysnc
+  for (let i = 0; i < data.length; i++) {
+    console.log(i);
+    // const item = data[i];
+    // zobacz, ty oprocz tej listy, z nazwa to jeszcze chcesz miec kilka innych rzeczy, ktore sa zawarte w tej tablicy ktora pobierasz z api,
+    // dlatego tutaj zamiast takiego zapisu const item = data[i];
+    // mozesz zrobic destrukturyzacje tej tablicy
+    // ul.innerHTML += `<li>${item.name}, ${item.country}</li>`;
+    const { name, lat, lon, country } = data[i];
+    ul.innerHTML += `<li
+    data-lat="${lat}"
+    data-lon="${lon}"
+    data-name="${name}"
+    > ${name} <span>${country}</span></li>`;
+  }
 }
 
 const debounceSearch = _.debounce(() => {
