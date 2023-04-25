00fc534a8228e652d9496048a668affa4a9b99ee
Marcin Soltysik <profittax@gmail.com>
2023-04-25 22:41:08 +0200

    feat(weather-app): Add layout weather

--- 02-weather-app/weather.js
+++ 02-weather-app/weather.js
@@ -8,18 +8,12 @@ async function search() {
   const response = await fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${phrase}&limit=5&appid=${key}`
   );
-  const data = await response.json(); // pobrales dane, teraz cos trzeba z nimi zrobic
+  const data = await response.json();
   console.log(data);
   const ul = document.querySelector('.input--list');
-  ul.innerHTML = ''; //czyscisz liste
-  // teraz iteracja po tablicy z danymi ktore pobrales w aysnc
+  ul.innerHTML = '';
   for (let i = 0; i < data.length; i++) {
     console.log(i);
-    // const item = data[i];
-    // zobacz, ty oprocz tej listy, z nazwa to jeszcze chcesz miec kilka innych rzeczy, ktore sa zawarte w tej tablicy ktora pobierasz z api,
-    // dlatego tutaj zamiast takiego zapisu const item = data[i];
-    // mozesz zrobic destrukturyzacje tej tablicy
-    // ul.innerHTML += `<li>${item.name}, ${item.country}</li>`;
     const { name, lat, lon, country } = data[i];
     ul.innerHTML += `<li
     data-lat="${lat}"
