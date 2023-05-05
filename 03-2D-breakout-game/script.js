4d21a5b18a55bc8c92a50074065ed09da1729f7b
Marcin Soltysik <profittax@gmail.com>
2023-05-05 23:38:47 +0200

    feat(2D-breakout-game): Add script file with example piece of code that prints a red square on the canvas.

---
+++ 03-2D-breakout-game/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const canvas = document.getElementById('myIdCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = '#FF0000';
+ctx.fill();
+ctx.closePath();
