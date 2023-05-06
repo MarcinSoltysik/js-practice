6d4ce963ba1e8561694bdedcb53e5f4c5ed41b3c
Marcin Soltysik <profittax@gmail.com>
2023-05-06 23:50:33 +0200

    feat(2D-breakout-game): Clean Up cod, Add to separate function, drawBall, draw

--- 03-2D-breakout-game/script.js
+++ 03-2D-breakout-game/script.js
@@ -5,8 +5,8 @@ const ctx = canvas.getContext('2d');
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
+let dx = -2.3;
+let dy = -3;
 
 function drawBall() {
   ctx.beginPath();
