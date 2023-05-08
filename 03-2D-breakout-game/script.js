93586f74b7721c87dae906e7cf6aecbe8709209e
Marcin Soltysik <profittax@gmail.com>
2023-05-08 07:43:08 +0200

    feat(2D-breakout-game): Add functions: draw the paddle, keyHandler for keyboard controls

--- 03-2D-breakout-game/script.js
+++ 03-2D-breakout-game/script.js
@@ -9,6 +9,24 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
+// paddle
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+// user to control the paddle
+let rightPressed = false;
+let leftPressed = false;
+
+// drawPaddle
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -20,15 +38,9 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   x += dx;
   y += dy;
-  // if (x + dx > canvas.width || x + dx < 0) {
-  //   dx = -dx;
-  // }
-
-  // if (y + dy > canvas.height || y + dy < 0) {
-  //   dy = -dy;
-  // }
 
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx;
@@ -36,6 +48,47 @@ function draw() {
   if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
     dy = -dy;
   }
+
+  // This currently works, but the paddle disappears off the edge of the canvas if we hold either key for too long.
+
+  // if (rightPressed) {
+  //   paddleX += 7;
+  // } else if (leftPressed) {
+  //   paddleX -= 7;
+  // }
+
+  if (rightPressed) {
+    paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+  } else if (leftPressed) {
+    paddleX = Math.max(paddleX - 7, 0);
+  }
 }
 
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = true;
+  }
+  console.log(e);
+}
+
+function keyUpHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = false;
+  }
+}
+
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', function () {
+//   setInterval(draw, 10);
+// });
+
 setInterval(draw, 10);
+
+// function draw the paddle, keyboard controls
