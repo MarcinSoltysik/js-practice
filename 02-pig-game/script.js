959906c46506390e173a7f39ead3621561765d02
Marcin Soltysik <profittax@gmail.com>
2023-07-06 22:22:09 +0200

    Add switchPlayer function

--- 02-pig-game/script.js
+++ 02-pig-game/script.js
@@ -15,6 +15,13 @@ let playing = true;
 let currentScore;
 let activePlayer;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 // Rolling dice
 
 btnRoll.addEventListener('click', function () {
