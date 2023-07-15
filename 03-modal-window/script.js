3276acf37399ba02d5072e92779bb2f981b59e7d
Marcin Soltysik <profittax@gmail.com>
2023-07-15 11:35:06 +0200

    feat(modal-window): Add remove class hidden to show hiden modal

--- 03-modal-window/script.js
+++ 03-modal-window/script.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+
 const btnsOpenModal = document.querySelectorAll('.btn__show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', function () {
     console.log(btnsOpenModal[i], `modal:${i}`);
+    modal.classList.remove('hidden');
   });
 }
