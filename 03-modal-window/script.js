152c86537a9d31e6536720a35708606ddf3b0258
Marcin Soltysik <profittax@gmail.com>
2023-07-15 22:54:55 +0200

    feat(modal-window): Add remove and add class hidden to show and hidden overlay

--- 03-modal-window/script.js
+++ 03-modal-window/script.js
@@ -1,16 +1,18 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
-
+const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.btn__show-modal');
 const btnCloseModal = document.querySelector('.btn__close-modal');
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', function () {
     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 }
 
 btnCloseModal.addEventListener('click', function () {
   modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 });
