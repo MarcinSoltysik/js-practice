2bb3bcdda06fd9f2265558ed7eb26dc6056cd71a
Marcin Soltysik <profittax@gmail.com>
2023-07-15 23:04:56 +0200

    feat(modal-window): Add close modal when press Escape button

--- 03-modal-window/script.js
+++ 03-modal-window/script.js
@@ -23,3 +23,11 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 // close modal
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// close modal keydown ESC
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
