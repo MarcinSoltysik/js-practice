ed26b828256caf9fba628550a9dabfbfff7ddd8c
Marcin Soltysik <profittax@gmail.com>
2023-09-23 23:15:13 +0200

    feat(shop-js): FIX css

--- 05-shop-JS/src/js/app.js
+++ 05-shop-JS/src/js/app.js
@@ -4,6 +4,7 @@
 const product1 = { name: 'koszula', price: 10 };
 const product2 = { name: 'marynarka', price: '20' };
 const discount = 10;
+let discountEnable = false;
 
 if (typeof product2.price === 'string') {
   console.log('Błędny typ danych');
@@ -28,46 +29,30 @@ addItem(product1);
 addItem(product2);
 
 // cena całkowita
-const total = Number(product1.price) + Number(product2.price);
-const totalWithDiscount = total - discount;
 
-document.querySelector('#total-price').innerHTML = totalWithDiscount;
+function calculatePrice() {
+  const total = Number(product1.price) + Number(product2.price);
+
+  let totalWithDiscount = total;
+  if (discountEnable) {
+    totalWithDiscount = total - discount;
+  }
+
+  document.querySelector('#total-price').innerHTML = totalWithDiscount;
+}
 
 // dodawnia znizki przy checkobx,
 
-function addDiscount() {
+function addDiscount(e) {
+  discountEnable = e.target.checked;
+
   if (discount > 0) {
     document.querySelector('#discount-amount').innerHTML = -discount;
     document.querySelector('#discount').classList.toggle('hidden');
   }
+  calculatePrice();
 }
 
-// zniżka, wyświetlanie / ukrywanie
+document.querySelector('#add-discount').addEventListener('click', addDiscount);
 
-// const discountContainer = document.querySelector('#discount-amount');
-// discountContainer.innerHTML = -discount;
-
-// if (discount > 0) {
-//   document.querySelector('#discount-amount').innerHTML = -discount;
-//   document.querySelector('#discount').classList.remove('hidden');
-// }
-
-/////////////////////////////
-/// test log
-///
-//  TEST console.log
-///
-console.log(total);
-console.log(totalWithDiscount);
-
-console.log(
-  `cena przed znizką: ${total}, cena po znizce: ${totalWithDiscount}`
-);
-
-console.log(typeof product1.price, typeof product2.price);
-
-console.log(typeof '20');
-console.log(typeof Number('20'));
-
-//  TEST console.log
-console.log(document.querySelector('#total-price'));
+calculatePrice();
