6006a0ea69b247baf735a0bf4ad143db9df9e3d0
Marcin Soltysik <profittax@gmail.com>
2023-11-25 21:11:07 +0100

    feat(self-driving-car): Add left, right controls

--- 04-self-driving-car/car.js
+++ 04-self-driving-car/car.js
@@ -38,6 +38,13 @@ class Car {
       this.speed = 0;
     }
 
+    if (this.controls.left) {
+      this.x -= 2;
+    }
+    if (this.controls.right) {
+      this.x += 2;
+    }
+
     this.y -= this.speed;
   }
 
