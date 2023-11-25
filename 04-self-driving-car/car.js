9e70bee3e43f42041ea0a1ae09e36c89f2bf0ab3
Marcin Soltysik <profittax@gmail.com>
2023-11-25 23:54:13 +0100

    feat(self-driving-car):Add move method, separate code

--- 04-self-driving-car/car.js
+++ 04-self-driving-car/car.js
@@ -16,44 +16,50 @@ class Car {
   }
 
   update() {
-    if (this.controls.forward) {
-      this.speed += this.acceleration;
-    }
-    if (this.controls.reverse) {
-      this.speed -= this.acceleration;
-    }
-    if (this.speed > this.maxSpeed) {
-      this.speed = this.maxSpeed;
-    }
-    if (this.speed < -this.maxSpeed / 2) {
-      this.speed = -this.maxSpeed / 2;
-    }
+    this.#move();
+  }
 
-    if (this.speed > 0) {
-      this.speed -= this.friction;
-    }
-
-    if (this.speed < 0) {
-      this.speed += this.friction;
-    }
-
-    if (Math.abs(this.speed) < this.friction) {
-      this.speed = 0;
-    }
-
-    if (this.speed != 0) {
-      const flip = this.speed > 0 ? 1 : -1;
-
-      if (this.controls.left) {
-        this.angle += 0.03 * flip;
+  #move() {
+    {
+      if (this.controls.forward) {
+        this.speed += this.acceleration;
       }
-      if (this.controls.right) {
-        this.angle -= 0.03 * flip;
+      if (this.controls.reverse) {
+        this.speed -= this.acceleration;
       }
-    }
+      if (this.speed > this.maxSpeed) {
+        this.speed = this.maxSpeed;
+      }
+      if (this.speed < -this.maxSpeed / 2) {
+        this.speed = -this.maxSpeed / 2;
+      }
 
-    this.x -= Math.sin(this.angle) * this.speed;
-    this.y -= Math.cos(this.angle) * this.speed;
+      if (this.speed > 0) {
+        this.speed -= this.friction;
+      }
+
+      if (this.speed < 0) {
+        this.speed += this.friction;
+      }
+
+      if (Math.abs(this.speed) < this.friction) {
+        this.speed = 0;
+      }
+
+      if (this.speed != 0) {
+        const flip = this.speed > 0 ? 1 : -1;
+
+        if (this.controls.left) {
+          this.angle += 0.03 * flip;
+        }
+        if (this.controls.right) {
+          this.angle -= 0.03 * flip;
+        }
+      }
+
+      this.x -= Math.sin(this.angle) * this.speed;
+      this.y -= Math.cos(this.angle) * this.speed;
+    }
   }
 
   draw(ctx) {
