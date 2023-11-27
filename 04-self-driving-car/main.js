f0008528f2e8b87f5f2629c9fe424334ff53c29c
Marcin Soltysik <profittax@gmail.com>
2023-11-27 18:43:43 +0100

    feat(self-driving-car): Add bestCar to localStorage

--- 04-self-driving-car/main.js
+++ 04-self-driving-car/main.js
@@ -11,9 +11,22 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 // const car = new Car(road.getLaneCenter(1), 100, 30, 50, 'AI');
 const N = 100;
 const cars = generateCars(N);
+let bestCar = cars[0];
+if (localStorage.getItem('bestBrain')) {
+  bestCar.brain = JSON.parse(localStorage.getItem('bestBrain'));
+}
+
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)];
 
 animate();
+
+function save() {
+  localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+  localStorage.removeItem('bestBrain');
+}
 
 function generateCars(N) {
   const cars = [];
