09f2d6526f0a97fe278d8bb56401dee0bffa66ce
Marcin Soltysik <profittax@gmail.com>
2023-11-27 18:59:22 +0100

    feat(self-driving-car): Add more traffic car

--- 04-self-driving-car/main.js
+++ 04-self-driving-car/main.js
@@ -16,7 +16,11 @@ if (localStorage.getItem('bestBrain')) {
   bestCar.brain = JSON.parse(localStorage.getItem('bestBrain'));
 }
 
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)];
+const traffic = [
+  new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2),
+  new Car(road.getLaneCenter(0), -300, 30, 50, 'DUMMY', 2),
+  new Car(road.getLaneCenter(2), -100, 30, 50, 'DUMMY', 2),
+];
 
 animate();
 
