0810a700dbc36c8e066fb48a235e68870f097bd6
Marcin Soltysik <profittax@gmail.com>
2023-12-01 21:33:45 +0100

    feat(tetris): Add moveLeft function

--- 05-tetris-game/app.js
+++ 05-tetris-game/app.js
@@ -97,4 +97,23 @@ document.addEventListener('DOMContentLoaded', () => {
       draw();
     }
   }
+
+  // move left
+
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
+    if (!isAtLeftEdge) currentPosition -= 1;
+
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains('taken')
+      )
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
 });
